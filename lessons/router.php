98a04fa63a7693ac1c98015c04011b0a5e4a1ec1
Abraham Aquino <abrahamdic@hotmail.com>
2019-08-19 01:30:03 -0500

    feat(ajax) save new book, add validation in router

--- lessons/router.php
+++ lessons/router.php
@@ -1,6 +1,13 @@
 <?php
 
 $matches = [];
+
+if ( in_array( $_SERVER["REQUEST_URI"], [ '/index.html', '/', '' ] ) ) {
+    echo file_get_contents( '../api_rest_and_ajax/index.html' );
+    
+    die;
+}
+
 if ( preg_match('/\/([^\/]+)\/([^\/]+)/', $_SERVER['REQUEST_URI'], $matches) ) {
     $_GET['resource_type'] = $matches[1];
     $_GET['resource_id'] = $matches[2];
