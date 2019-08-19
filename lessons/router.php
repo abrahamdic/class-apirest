5552f28240fffa53619d36d654a5ddeecbdf7be7
Abraham Aquino <abrahamdic@hotmail.com>
2019-08-18 19:33:14 -0500

    feat(auth) add auth with TOKEN

--- lessons/router.php
+++ lessons/router.php
@@ -6,11 +6,11 @@ if ( preg_match('/\/([^\/]+)\/([^\/]+)/', $_SERVER['REQUEST_URI'], $matches) ) {
     $_GET['resource_id'] = $matches[2];
 
     error_log( print_r($matches, 1) );
-    require 'server_WITH_AUTH_HMAC.php';
+    require 'server_WITH_AUTH_TOKEN.php';
 } else if ( preg_match('/\/([^\/]+)\/?/', $_SERVER['REQUEST_URI'], $matches) ) {
     $_GET['resource_type'] = $matches[1];
     error_log( print_r($matches, 1) );
-    require 'server_WITH_AUTH_HMAC.php';
+    require 'server_WITH_AUTH_TOKEN.php';
 } else {
     error_log('No Matches');
     http_response_code( 404 );
